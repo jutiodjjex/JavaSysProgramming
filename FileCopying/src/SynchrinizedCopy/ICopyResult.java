e79e2734434dd55af8b43789b91d0f01a903208b
jutiodjjex <40070146+jutiodjjex@users.noreply.github.com>
2018-12-25 18:23:24 +0300

    11

--- FileCopying/src/SynchrinizedCopy/ICopyResult.java
+++ FileCopying/src/SynchrinizedCopy/ICopyResult.java
@@ -22,13 +22,7 @@ public class ICopyResult extends Thread {
         OutputStream os = null;
         try {
             is = new FileInputStream(source);
-            System.out.printf("%s \n", source);
             os = new FileOutputStream(destination);
-            System.out.printf("%s \n", destination);
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = is.read(buffer)) > 0) {
-                os.write(buffer, 0, length);
             }
         } catch (IOException e) {
             e.printStackTrace();
--- NIOFileMoving/src/FileMoving/Main.java
+++ NIOFileMoving/src/FileMoving/Main.java
@@ -3,6 +3,9 @@ package FileMoving;
 import java.io.IOException;
 import java.nio.file.*;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.lang.String;
 
 
 /**
@@ -19,17 +22,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        int moveCount = 0;
 
         System.out.println("Введите папку, из которой нужно скопировать файлы: ");
-        Path sourceDirectory = Paths.get(scan.nextLine());
+        String sourceDirectory = scan.nextLine();
 
         System.out.println("Введите папку, в которую нужно скопировать файлы: ");
-        Path destinationDirectory = Paths.get(scan.nextLine());
+        String destinationDirectory = scan.nextLine();
 
-        moveCount = fileMovingNCounting(sourceDirectory, destinationDirectory, moveCount);
+        fileMoving(sourceDirectory, destinationDirectory);
+        movedFilesNotify();
 
-        System.out.println(moveCount + " files was successfully moved to " + destinationDirectory + " folder." );
     }
 
 
@@ -43,21 +45,66 @@ public class Main {
      *
      * @param srcDirectory - путь к директории, откуда копируются файлы.
      * @param destDirectory - путь к директории, куда копируются файлы.
-     * @param moveCount - кол-во перемещённых файлов.
-     * @return - возвращает кол-во перемещённых файлов после окончания итераций цикла.
      */
-    static private int fileMovingNCounting(Path srcDirectory, Path destDirectory, int moveCount){
-        try (DirectoryStream<Path> stream = Files.newDirectoryStream(srcDirectory)) {
+    static private void fileMoving(String srcDirectory, String destDirectory){
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(Paths.get(srcDirectory))) {
             for (Path entry : stream) {
-                moveCount++;
-                Files.move(entry, Paths.get(destDirectory.toString() + "\\" + entry.getFileName()));
-                System.out.println("File \"" + entry.getFileName() + "\" \nWas moved to \"" + destDirectory.getParent() + "\" folder \n");
+                Files.move(entry, Paths.get(destDirectory + "\\" + entry.getFileName()));
+                String transferedFileName = "File \"" + entry.getFileName() + "\" was moved.";
+                saveNameOfTransferedFile(transferedFileName);
 
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return moveCount;
+    }
+
+    static private void saveNameOfTransferedFile(String nameOfTheTransferedFile){
+        try(FileWriter writer = new FileWriter("C:\\Users\\PryaN\\IdeaProjects\\JavaSysProgramming\\NIOFileMoving\\src\\FileMoving\\MovedFiles.txt", true)) {
+
+            writer.write(nameOfTheTransferedFile);
+            writer.append('\n');
+            writer.flush();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    static private void movedFilesNotify(){
+
+        countOfTransferedFiles();
+
+        try(FileReader reader = new FileReader("C:\\Users\\PryaN\\IdeaProjects\\JavaSysProgramming\\NIOFileMoving\\src\\FileMoving\\MovedFiles.txt"))
+        {
+            int eachCharReader;
+            while((eachCharReader = reader.read()) != -1){
+                System.out.print((char)eachCharReader);
+            }
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static private void countOfTransferedFiles(){
+        int filesMovedCount = 0;
+        try(FileReader reader = new FileReader("C:\\Users\\PryaN\\IdeaProjects\\JavaSysProgramming\\NIOFileMoving\\src\\FileMoving\\MovedFiles.txt");
+            FileWriter writer = new FileWriter("C:\\Users\\PryaN\\IdeaProjects\\JavaSysProgramming\\NIOFileMoving\\src\\FileMoving\\MovedFiles.txt", true))
+        {
+            int eachCharReader;
+            while((eachCharReader = reader.read()) != -1){
+                if(eachCharReader == '\n'){
+                    filesMovedCount++;
+                }
+            }
+            writer.write("Files moved: " + filesMovedCount);
+            writer.flush();
+        }
+        catch(IOException e){
+            System.out.println(e.getMessage());
+        }
     }
 }
 
